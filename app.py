4a10b7d115a3d7968ad5541e07480cd9029e81d0
Vincent <vincentwarmerdam@gmail.com>
2021-12-20 10:35:09 +0100

    added

--- app.py
+++ app.py
@@ -1,65 +1,16 @@
-import json
-import random 
-
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
-from pydantic import BaseModel, validator
-
-from fastapi.templating import Jinja2Templates
-
-templates = Jinja2Templates(directory="templates")
+import random
+from bulk import create_app
 
 
 def batch_generator():
+    """Generates a batch of strings to label."""
     letters = "qwertyuiopasdfghjklzxcvbnm"
     while True:
         strings = ["".join(random.choice(letters) for i in range(5)) for j in range(6)]
         original_id = "".join(random.choice(letters) for i in range(10))
         yield {"original_id": original_id, "items": strings}
 
+
 gen = batch_generator()
 
-
-def _parse_rq_items(items):
-    """Takes the items from the query-string and turns them into a list of labelled dictionaries"""
-    original_id = [i for i in items if i[0] == 'origid'][0][1]
-    items = [i for i in items if i[0] != "origid"]
-    selected = set([i[0] for i in items])
-    checked = set([i[0] for i in items if i[1] == 'on'])
-    unchecked = selected.difference(checked)
-
-    results = []
-    for item in checked:
-        results.append({"item": item, "original_id": original_id, "label": 1})
-    for item in unchecked:
-        results.append({"item": item, "original_id": original_id, "label": 0})
-    return results
-
-
-def create_app(batch_generator=batch_generator(), outfile="labels.jsonl"):
-    app = FastAPI()
-
-    @app.get("/items/{id}", response_class=HTMLResponse)
-    async def read_item(request: Request, id: str):
-        batch = batch_generator()
-        context = {"request": request, "strings": batch['items'], "id": id, "orig_id": batch['original_id']}
-        return templates.TemplateResponse("labelview.html", context=context)
-
-
-    @app.post("/submit")
-    async def submit(request: Request, response_class=HTMLResponse):
-        body = (await request.body()).decode("utf-8")
-        items = [s.split("=") for s in body.split("&")]
-
-        # Store the labels locally
-        labels = _parse_rq_items(items)
-        with open(outfile, "a") as f:
-            for label in labels:
-                f.write(json.dumps(label) + "\n")
-        
-        # Make a new batch for htmx
-        batch = batch_generator()
-        context = {"request": request, "strings": batch['items'], "id": id, "orig_id": batch['original_id']}
-        return templates.TemplateResponse("labeltable.html", context=context)
-    
-    return app
+app = create_app(batch_generator=gen)
