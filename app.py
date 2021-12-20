953739bd889a313399a817b7e8322786fbfd2a95
Vincent <vincentwarmerdam@gmail.com>
2021-12-20 11:02:33 +0100

    base idea works now

--- app.py
+++ app.py
@@ -6,7 +6,9 @@ def batch_generator():
     """Generates a batch of strings to label."""
     letters = "qwertyuiopasdfghjklzxcvbnm"
     while True:
-        strings = ["".join(random.choice(letters) for i in range(5)) for j in range(6)]
+        strings = [
+            "".join(random.choice(letters) for i in range(50)) for j in range(10)
+        ]
         original_id = "".join(random.choice(letters) for i in range(10))
         yield {"original_id": original_id, "items": strings}
 
