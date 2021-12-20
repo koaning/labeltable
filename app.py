94b4a2fd359e455b402aa3110a1a69d773720e59
Vincent <vincentwarmerdam@gmail.com>
2021-12-20 10:16:55 +0100

    added app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 import random 
 
 from fastapi import FastAPI, Request
@@ -5,8 +6,6 @@ from fastapi.responses import HTMLResponse
 from pydantic import BaseModel, validator
 
 from fastapi.templating import Jinja2Templates
-
-app = FastAPI()
 
 templates = Jinja2Templates(directory="templates")
 
@@ -20,7 +19,26 @@ def batch_generator():
 
 gen = batch_generator()
 
-def create_app(batch_generator=batch_generator()):
+
+def _parse_rq_items(items):
+    """Takes the items from the query-string and turns them into a list of labelled dictionaries"""
+    original_id = [i for i in items if i[0] == 'origid'][0][1]
+    items = [i for i in items if i[0] != "origid"]
+    selected = set([i[0] for i in items])
+    checked = set([i[0] for i in items if i[1] == 'on'])
+    unchecked = selected.difference(checked)
+
+    results = []
+    for item in checked:
+        results.append({"item": item, "original_id": original_id, "label": 1})
+    for item in unchecked:
+        results.append({"item": item, "original_id": original_id, "label": 0})
+    return results
+
+
+def create_app(batch_generator=batch_generator(), outfile="labels.jsonl"):
+    app = FastAPI()
+
     @app.get("/items/{id}", response_class=HTMLResponse)
     async def read_item(request: Request, id: str):
         batch = batch_generator()
@@ -32,19 +50,16 @@ def create_app(batch_generator=batch_generator()):
     async def submit(request: Request, response_class=HTMLResponse):
         body = (await request.body()).decode("utf-8")
         items = [s.split("=") for s in body.split("&")]
+
+        # Store the labels locally
+        labels = _parse_rq_items(items)
+        with open(outfile, "a") as f:
+            for label in labels:
+                f.write(json.dumps(label) + "\n")
         
-        original_id = [i for i in items if i[0] == 'origid'][0][1]
-        items = [i for i in items if i[0] != "origid"]
-        selected = set([i[0] for i in items])
-        checked = set([i[0] for i in items if i[1] == 'on'])
-        unchecked = selected.difference(checked)
-
-        for item in checked:
-            print(f"{item},{original_id},1")
-        for item in unchecked:
-            print(f"{item},{original_id},0")
-
+        # Make a new batch for htmx
         batch = batch_generator()
         context = {"request": request, "strings": batch['items'], "id": id, "orig_id": batch['original_id']}
         return templates.TemplateResponse("labeltable.html", context=context)
-
+    
+    return app
