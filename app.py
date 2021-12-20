6bbb8be2eefc80707eac5e97964ed525df8d014d
Vincent <vincentwarmerdam@gmail.com>
2021-12-20 10:03:11 +0100

    pr

--- app.py
+++ app.py
@@ -11,37 +11,40 @@ app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
 
-
-@app.get("/items/{id}", response_class=HTMLResponse)
-async def read_item(request: Request, id: str):
+def batch_generator():
     letters = "qwertyuiopasdfghjklzxcvbnm"
-    strings = ["".join(random.choice(letters) for i in range(5)) for j in range(6)]
-    original_id = "".join(random.choice(letters) for i in range(10))
-    context = {"request": request, "strings": strings, "id": id, "orig_id": original_id}
-    return templates.TemplateResponse("labelview.html", context=context)
+    while True:
+        strings = ["".join(random.choice(letters) for i in range(5)) for j in range(6)]
+        original_id = "".join(random.choice(letters) for i in range(10))
+        yield {"original_id": original_id, "items": strings}
 
-class Item(BaseModel):
-    name: str
-    price: float
+gen = batch_generator()
 
-@app.post("/submit")
-async def submit(request: Request, response_class=HTMLResponse):
-    body = (await request.body()).decode("utf-8")
-    items = [s.split("=") for s in body.split("&")]
-    
-    original_id = [i for i in items if i[0] == 'origid'][0][1]
-    items = [i for i in items if i[0] != "origid"]
-    selected = set([i[0] for i in items])
-    checked = set([i[0] for i in items if i[1] == 'on'])
-    unchecked = selected.difference(checked)
+def create_app(batch_generator=batch_generator()):
+    @app.get("/items/{id}", response_class=HTMLResponse)
+    async def read_item(request: Request, id: str):
+        batch = batch_generator()
+        context = {"request": request, "strings": batch['items'], "id": id, "orig_id": batch['original_id']}
+        return templates.TemplateResponse("labelview.html", context=context)
 
-    for item in checked:
-        print(f"{item},{original_id},1")
-    for item in unchecked:
-        print(f"{item},{original_id},0")
 
-    letters = "qwertyuiopasdfghjklzxcvbnm"
-    strings = ["".join(random.choice(letters) for i in range(5)) for j in range(6)]
-    original_id = "".join(random.choice(letters) for i in range(10))
-    context = {"request": request, "strings": strings, "id": id, "orig_id": original_id}
-    return templates.TemplateResponse("labeltable.html", context=context)
+    @app.post("/submit")
+    async def submit(request: Request, response_class=HTMLResponse):
+        body = (await request.body()).decode("utf-8")
+        items = [s.split("=") for s in body.split("&")]
+        
+        original_id = [i for i in items if i[0] == 'origid'][0][1]
+        items = [i for i in items if i[0] != "origid"]
+        selected = set([i[0] for i in items])
+        checked = set([i[0] for i in items if i[1] == 'on'])
+        unchecked = selected.difference(checked)
+
+        for item in checked:
+            print(f"{item},{original_id},1")
+        for item in unchecked:
+            print(f"{item},{original_id},0")
+
+        batch = batch_generator()
+        context = {"request": request, "strings": batch['items'], "id": id, "orig_id": batch['original_id']}
+        return templates.TemplateResponse("labeltable.html", context=context)
+
